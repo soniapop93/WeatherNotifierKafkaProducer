3a64460077e4dc72f486f7448c5e27a2661e1133
soniapop93 <popsonia85@gmail.com>
2023-06-13 22:17:40 +0300

    first commit

---
+++ WeatherNotifierKafkaProducer/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        /*
+           =============================================================
+           =============================================================
+              The API endoint used in this script is free to use.
+              https://api.techniknews.net/ipgeo/ + ipAddress
+              https://ipinfo.io/ip
+
+           =============================================================
+           =============================================================
+        */
+
+    }
+}
