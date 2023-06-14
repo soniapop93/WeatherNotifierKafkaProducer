d0ae6dfb06d8902996a594a8f70d9415a934468b
soniapop93 <popsonia85@gmail.com>
2023-06-14 22:12:06 +0300

    started to work on the weather api

---
+++ WeatherNotifierKafkaProducer/Weather/RequestsWeather.cs
@@ -0,0 +1,19 @@
+﻿using WeatherNotifierKafkaProducer.Utilities;
+
+namespace WeatherNotifierKafkaProducer.Weather
+{
+    public class RequestsWeather
+    {
+        public void weatherInfo()
+        {
+            getWeatherInfo();
+        }
+
+        private void getWeatherInfo(string latitutude, string longitude)
+        {
+            string endpoint = "https://api.open-meteo.com/v1/forecast?latitude=" + latitutude + "&longitude=" + longitude + "&hourly=precipitation_probability&current_weather=true";
+            RequestManager.getRequest(endpoint);
+        }
+
+    }
+}
