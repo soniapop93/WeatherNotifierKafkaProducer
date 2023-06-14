f67d6ec288cf21475eac837d007613e32c7b570d
soniapop93 <popsonia85@gmail.com>
2023-06-14 23:36:37 +0300

    added requirments in the program file

--- WeatherNotifierKafkaProducer/Program.cs
+++ WeatherNotifierKafkaProducer/Program.cs
@@ -7,10 +7,17 @@ public class Program
         /*
            =============================================================
            =============================================================
-              The API endoint used in this script is free to use.
-              https://api.techniknews.net/ipgeo/ + ipAddress
+              The API endoints used in this script are free to use.
               https://ipinfo.io/ip
+              https://api.techniknews.net/ipgeo/ + ipAddress
               https://api.open-meteo.com/
+
+            Kafka Producer:
+                - uses this weather free API: 
+                https://api.open-meteo.com/v1/forecast?latitude=$value&longitude=$value&hourly=precipitation_probability&current_weather=true
+                - it makes requests on the API every 5 mins to get new data
+                - checks the weather for the current hour
+                - if the precipitation probability changed for the current hour will creates a message to be sent to Kafka
 
            =============================================================
            =============================================================
