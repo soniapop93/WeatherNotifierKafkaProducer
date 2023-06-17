c685bb63f7b939b244f9c1b95797688ecef6a7ed
soniapop93 <popsonia85@gmail.com>
2023-06-17 15:08:35 +0300

    started to implement the kafka producer. didn't test it yet

---
+++ WeatherNotifierKafkaProducer/Kafka/Producer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using System.Net;
+using System.ComponentModel;
+
+namespace WeatherNotifierKafkaProducer.Kafka
+{
+    public class Producer
+    {
+        ProducerConfig config = new ProducerConfig
+        {
+            BootstrapServers = ""
+        };
+
+        private const string topicName = "";
+
+        public async void sendMessages (string message)
+        {
+            IProducer<string, string> producer = new ProducerBuilder<string, string>(config).Build();
+
+            DeliveryResult<string, string> sendMessage = await producer.ProduceAsync(topicName, new Message<string, string> { Key = "", Value = message });
+            Console.WriteLine("Message sent '{sendMessage.Value}' to '{sendMessage.TopicPartitionOffset}'");
+        }
+        
+    }
+}
